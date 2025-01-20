0016c3e6cfbda5db569d5c249a415cd22bb8b41b
arashthegeek <arashthegeek@gmail.com>
2025-01-20 22:39:03 +0330

    making index page(where user put its link and...) view, get parameters, and basic if's

--- djangowebapp/Website/views.py
+++ djangowebapp/Website/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    link = request.GET.get('link', None)
+    is_slugcustom = request.GET.get('is_slugcustom', None)
+    custom_slug = request.GET.get('customslug', None)
+    if link is not None:
+        #making page with random slug
+        if is_slugcustom == 'on' and custom_slug is not None:
+            pass
+            #making page with custom slug
     return render(request,'index.html',)
